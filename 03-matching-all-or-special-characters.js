333dded4bdd21b2ac6fac68fd9ac6b9ab8f27677
Annie Huang <annie.hn@gmail.com>
2020-01-02 13:44:46 +1100

    \n, (x)

--- 03-matching-all-or-special-characters.js
+++ 03-matching-all-or-special-characters.js
@@ -127,9 +127,24 @@ let pwRegex = /(?=\w{5})(?=\D*\d{2})/;      // why \w{5} not meaning 5 words?? h
 console.log("'astronaut' with /(?=\\w{5})(?=\\D*\\d{2})/", pwRegex.test(sampleWord));
 
 
+// #############################################################################
+// Reuse Patterns Using Capture Groups
+// \n                   A back reference to the last substring matching the n parenthetical in the regular expression
+//                      (counting left parentheses), where "n" is a positive integer,
+// (x)                  Matches "x" and remembers the match, as the following example shows.
+//                      The parentheses are called capturing parentheses.
+//                      The (foo) and (bar) in the pattern /(foo) (bar) \1 \2/ match and remember the first two words in the string "foo bar foo bar".
+let repeatStr = 'regex abc';
+let repeatRegex = /(\w+)\s\1/;       // This is to find any number words, follow by space, and the follow by the same words.
+                                     // \1 refer to the first () content.
+console.log("'regex regex' with /(\\w)\\s\\1/", repeatRegex.test(repeatStr)); // Return true;
+// Returns ["regex regex", "regex"], The first item in the return list is full regex. The second one is the capture group.
+console.log("'regex regex' with /(\\w)\\s\\1/", repeatStr.match(repeatRegex));
 
-
-
+let repeatNum = '42 42 42';
+let reRegex = /^(\d+)\s\1\s\1$/; // Only want to find numbers that repeat exactly 3 times and separated by space.
+                                 // For me, this also restricted the not to be able to have 3 same number in the middle, like 'a 42 42 42 a'
+console.log("'42 42 42' with /(\\d+)\\s\\1\\s\\1/", reRegex.test(repeatNum));
 
 
 
